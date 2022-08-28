76ba5a74308a2a7082c65994e02669b7ec907993
Duarte Vicente Drumond <duartevicentedrumond@gmail.com>
2022-08-28 01:02:16 +0100

    Update dateToString fucntion to retrieve correct month number

--- src/components/Transaction/TransactionForm.js
+++ src/components/Transaction/TransactionForm.js
@@ -59,7 +59,7 @@ export default function TransactionForm() {
     function dateToString(date) {
 
       const year = date.getFullYear();
-      const month = ('0' + date.getMonth()).slice(-2);
+      const month = ('0' + (date.getMonth() + 1)).slice(-2);
       const day = ('0' + date.getDate()).slice(-2);
       const dateString = year + "-" + month + "-" + day;
 
