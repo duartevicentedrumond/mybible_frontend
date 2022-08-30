794ae2fb3b4423d9c13f2b385dcf1db454ebef43
Duarte Vicente Drumond <duartevicentedrumond@gmail.com>
2022-08-30 18:06:32 +0100

    Update chart x axis labels

--- src/components/Transaction/Components/LineGraph.js
+++ src/components/Transaction/Components/LineGraph.js
@@ -10,51 +10,51 @@ export default function LineGraph(data) {
         
         switch (sum.month) {
             case 1:
-                sum.date = "january " + sum.year;
+                sum.date = sum.year + "-01";
                 break;
 
             case 2:
-                sum.date = "february " + sum.year;
+                sum.date = sum.year + "-02";
                 break;
             
             case 3:
-                sum.date = "march " + sum.year;
+                sum.date = sum.year + "-03";
                 break;
 
             case 4:
-                sum.date = "april " + sum.year;
+                sum.date = sum.year + "-04";
                 break;
 
             case 5:
-                sum.date = "may " + sum.year;
+                sum.date = sum.year + "-05";
                 break;
 
             case 6:
-                sum.date = "june " + sum.year;
+                sum.date = sum.year + "-06";
                 break;
             
             case 7:
-                sum.date = "july " + sum.year;
+                sum.date = sum.year + "-07";
                 break;
 
             case 8:
-                sum.date = "august " + sum.year;
+                sum.date = sum.year + "-08";
                 break;
 
             case 9:
-                sum.date = "september " + sum.year;
+                sum.date = sum.year + "-09";
                 break;
 
             case 10:
-                sum.date = "october " + sum.year;
+                sum.date = sum.year + "-10";
                 break;
 
             case 11:
-                sum.date = "november " + sum.year;
+                sum.date = sum.year + "-11";
                 break;
 
             case 12:
-                sum.date = "december " + sum.year;
+                sum.date = sum.year + "-12";
                 break;
         
             default:
